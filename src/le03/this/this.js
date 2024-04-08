b53293086cf391a00a9bee39f14f1d87e3ae3a8e
Maximilian Tschoner <maximilian@tschoner.at>
2024-04-08 13:22:36 +0200

    Lecture 4

--- src/le03/this/this.js
+++ src/le03/this/this.js
@@ -1,5 +1,39 @@
-document.getElementById('trigger').addEventListener('click', () => {
-    console.log(this); // Sollte true loggen
-});
+const obj = {
+    name: 'obj',
+    checkThis(ref) {
+        return this === ref; // true
+    }
+};
 
+console.log(obj.checkThis(obj));
+console.log(obj.checkThis(window));
+
+const arrowFunc = () => {
+    console.log('Arrow function this value', this === window); // true
+};
+
+function myFunc() {
+    console.log('Function this value', this === window); // true
+}
+
+
+arrowFunc();
+myFunc();
+
+
+function showThis() {
+    'use strict';
+    return this;
+}
+function showThis2() {
+    return this;
+}
+
+console.log('Response of showThis is:: ', showThis()); // undefined
+console.log('Response of showThis2 is:: ', showThis2()); // undefined
+
+
+document.getElementById('trigger').addEventListener('click', () => {
+    console.log('This in anonym a.f. listener: ', this === window); // true
+});
 
---
+++ src/le04/le04.js
@@ -0,0 +1,28 @@
+const data = {
+    student: {
+        id: 1,
+        name: 'John Doe',
+        age: 16,
+    },
+    courses: [
+        {
+            name: 'Math',
+            grade: 3,
+        },
+        {
+            name: 'English',
+            grade: 4,
+        },
+        {
+            name: 'PE',
+            grade: 2,
+        },
+        {
+            name: 'Music',
+            grade: 5,
+        },
+    ],
+}
+
+
+
